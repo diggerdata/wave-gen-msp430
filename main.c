daedf835addd10ca43f6affed82d12d87983b18e
Nathan Rosenberg <robotincad@gmail.com>
2018-10-10 16:38:44 -0400

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 
 typedef enum {DC, SIN, SQUARE, TRIANGLE} waveType;
 
-void ConvertADC(void);
-void DACSetValue(unsigned int dac_code);
+void adc_convert(void);
+void starttimerA2(void);
 
 unsigned char counter;              // Current location in sinWave array
 unsigned char sinWave[32] = {          // sinWave array, preset to values of sine
@@ -15,9 +15,11 @@ unsigned char sinWave[32] = {          // sinWave array, preset to values of sin
 
 
 unsigned int i;                     // Used for 'for' loops.
+volatile unsigned int in_wheel;
 int squareToggle = 1;
+waveType state = DC;
 
-void ConvertADC() {
+void adc_convert() {
     ADC12CTL0 &= ~ADC12SC; // clear the start bit
     ADC12CTL0 |= ADC12SC; // Sampling and conversion start
 }
@@ -25,69 +27,76 @@ void ConvertADC() {
 void main(void) {
     WDTCTL = WDTPW + WDTHOLD;                   // Stop WDT
 
-    waveType = DC; // set waveType to a default
+    REFCTL0 &= ~REFMSTR;
 
-    P1DIR |= BIT0;                              // P1.0 output
-    counter = 0;                                // Reset counter
+    P8SEL &= ~BIT0;
+    P8DIR |= BIT0;
+    P8OUT |= BIT0;
 
-    REFCTL0 &= ~REFMSTR;    // Reset REFMSTR to hand over control of
-    // internal reference voltages to
-    // ADC12_A control registers
-
-    // internal reference voltage of 1.5 for pot
-    ADC12CTL0 = ADC12ON | ADC12REFON | ADC12SHT0_9 | ADC12MSC;
-    // use mem1 to hold pot values
+    ADC12CTL0 = ADC12SHT0_9 | ADC12REFON | ADC12ON | ADC12MSC;
+    ADC12CTL1 = ADC12SHP | ADC12CONSEQ_1;
+    ADC12MCTL0 = ADC12SREF_1 + ADC12INCH_10;
     ADC12MCTL1 = ADC12SREF_0 + ADC12INCH_0 + ADC12EOS;
 
-    __delay_cycles(100); // delay to allow settling
-    ADC12CTL0 |= ADC12ENC; // enable conversion
+    __delay_cycles(100);
+    ADC12CTL0 |= ADC12ENC;
 
-    // Initialize Timer
-    TA2CCTL0 = CCIE;                               // CCR0 interrupt enabled
-    TA2CCTL1 = CCIE;                               // CCR1 interrupt enabled
-    TA2CCR0 = 256;                                 // Set PWM period to 256 clock ticks
-    TA2CCR1 = sinWave[counter];                        // Set first duty cycle value
-    TA2CTL = TASSEL_2 + MC_1 + TAIE + TACLR;     // SMCLK, upmode, enable interrupt, clear TA1R
+    ADC12IE = BIT1;
+    _BIS_SR(GIE);
 
-    _BIS_SR(LPM0_bits + GIE);                   // Enter LPM0 w/ interrupt
+    configDisplay();
+    configKeypad();
+    starttimerA2();
+    DACInit();
+
+    volatile int btn;
 
     while(1){
-        waveType = getKey();
+        btn = getKey();
+
+        if(btn == '1') {
+            state = DC;
+        }
+        else if(btn == '2') {
+            state = SIN;
+        }
+        else if(btn == '3') {
+            state = SQUARE;
+        }
+        else if(btn == '4') {
+            state = TRIANGLE;
+        }
     }
 }
 
 /**
  * TimerA0 interrupt service routine
  **/
-#pragma vector=TIMERA0_VECTOR
-__interrupt void TIMERA0_ISR(void) {
-    P1OUT |= BIT0;              // Set P1.0
-
-    potReading = ADC12MEM1;
-    switch(waveType){
-    case default:
-        // cry
-        break;
-    case DC:
-        TA2CCR1 = potReading/20; // sets the duty cycle to be between 0 and 4095/X, controlled with the pot
-        break;
-    case SIN:
-        TA2CCR1 = sinWave[counter];       // Set next duty cycle value
-        break;
-    case SQUARE:
-        if(counter%2 == 0){ // by changing the integer modulo, you can change the frequency of the wave.
-                            // when X = 2, it is changing at 80 Hz.
-            squareToggle = squareToggle*(-1);
-        }
-        TA2CCR1 = (squareToggle*potReading + 2048)/20; // set the next duty cycle value to either 0 or 4095/X
-        break;
-    case TRIANGLE:
-        if(counter >16){
-            TA2CCR1 = (32-counter)*potReading/328; // the duty cycle will climb up and down between 0 and 200
-        }
-        else{
-            TA2CCR1 = counter*potReading/328;
-        }
+#pragma vector=TIMER2_A0_VECTOR
+__interrupt void TimerA2_ISR(void) {
+    adc_convert();
+    switch(state){
+        case DC:
+            DACSetValue(in_wheel);
+            break;
+        case SIN:
+            DACSetValue(sinWave[counter]);       // Set next duty cycle value
+            break;
+        case SQUARE:
+            if(counter%2 == 0){ // by changing the integer modulo, you can change the frequency of the wave.
+                                // when X = 2, it is changing at 80 Hz.
+                squareToggle = squareToggle*(-1);
+            }
+            DACSetValue((squareToggle*in_wheel + 2048)/20); // set the next duty cycle value to either 0 or 4095/X
+            break;
+        case TRIANGLE:
+            if(counter >16){
+                DACSetValue((32-counter)*in_wheel/328); // the duty cycle will climb up and down between 0 and 200
+            }
+            else{
+                DACSetValue(counter*in_wheel/328);
+            }
+            break;
     }
     counter += 1;               // Add Offset to CCR0
     if ( counter == 32) {
@@ -95,16 +104,13 @@ __interrupt void TIMERA0_ISR(void) {
     }
 }
 
-/**
- * TimerA1 Interrupt Vector (TAIV) handler
- **/
-#pragma vector=TIMERA1_VECTOR
-__interrupt void TIMERA1_ISR(void) {
-    switch( TA2IV ) {
-        case  2:                // CCR1 interrupt
-            P1OUT &= ~BIT0;     // Clear P1.0 to determine duty cycle.
-            break;
-        default:
-            break;
-    }
+#pragma vector=ADC12_VECTOR
+__interrupt void ADC12_ISR(void) {
+    in_wheel = ADC12MEM1;
+}
+
+void starttimerA2(void) {
+    TA2CTL = TASSEL_1 + MC_1 + ID_0;
+    TA2CCR0 = 225;
+    TA2CCTL0 = CCIE;
 }
